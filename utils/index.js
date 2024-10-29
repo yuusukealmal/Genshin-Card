316df8ef55efca8f239f9f0819805b19ba49e787
侑介 <spark9487111@gmail.com>
2024-10-29 12:40:49 +0800

    weird

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,13 @@
 const md5 = require('md5')
 
 const randomStr = (length) => {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-   var charactersLength = characters.length;
-   for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 const randomInt = (min, max) => {
@@ -16,17 +16,17 @@ const randomInt = (min, max) => {
 }
 
 const getQueryParam = (data) => {
-	if ( data === undefined ) {
-		return "";
-	}
-	const arr = [];
-	for ( let key of Object.keys(data) ) {
-		arr.push( `${key}=${data[key]}` );
-	}
-	return arr.join("&");
+  if (data === undefined) {
+    return "";
+  }
+  const arr = [];
+  for (let key of Object.keys(data)) {
+    arr.push(`${key}=${data[key]}`);
+  }
+  return arr.join("&");
 }
 
-const getDS = (query, body="") => {
+const getDS = (query, body = "") => {
   // v2.11.1 - from app
   const n = "xV8v4Qu54lUKrEYFZkJhB8cuOh9Asafs";
   const i = Date.now() / 1000 | 0;
@@ -39,24 +39,24 @@ const getDS = (query, body="") => {
 
 const render = (template, context) => {
 
-    var tokenReg = /(\\)?\{\{([^\{\}\\]+)(\\)?\}\}/g;
+  var tokenReg = /(\\)?\{\{([^\{\}\\]+)(\\)?\}\}/g;
 
-    return template.replace(tokenReg, function (word, slash1, token, slash2) {
-        if (slash1 || slash2) {  
-            return word.replace('\\', '');
-        }
+  return template.replace(tokenReg, function (word, slash1, token, slash2) {
+    if (slash1 || slash2) {
+      return word.replace('\\', '');
+    }
 
-        var variables = token.replace(/\s/g, '').split('.');
-        var currentObject = context;
-        var i, length, variable;
+    var variables = token.replace(/\s/g, '').split('.');
+    var currentObject = context;
+    var i, length, variable;
 
-        for (i = 0, length = variables.length; i < length; ++i) {
-            variable = variables[i];
-            currentObject = currentObject[variable];
-            if (currentObject === undefined || currentObject === null) return '';
-        }
-        return currentObject;
-    });
+    for (i = 0, length = variables.length; i < length; ++i) {
+      variable = variables[i];
+      currentObject = currentObject[variable];
+      if (currentObject === undefined || currentObject === null) return '';
+    }
+    return currentObject;
+  });
 }
 
 module.exports = {
