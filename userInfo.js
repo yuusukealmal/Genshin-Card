ea93c89a6c10f763471eb5c324289fc5f44ba799
侑介 <spark9487111@gmail.com>
2024-11-03 16:56:30 +0800

    switch case game to get infos

--- userInfo.js
+++ userInfo.js
@@ -88,35 +88,44 @@ const userInfo = (game, uid, detail=false) => {
             .then(resp => {
               resp = JSON.parse(resp)
               if (resp.retcode === 0) {
-                if (detail){
-                  const { world_explorations } = resp.data
-                  const percentage = Math.min((world_explorations.reduce((total, next) => total + next.exploration_percentage, 0) / world_explorations.length / 10000 * 1000).toFixed(1), 100)
-                  const world_exploration = percentage
-  
-                  const data = {
-                    uid: game_role_id,
-                    world_exploration,
-                    ...resp.data.stats,
-                    ...roleInfo
-                  }
-                  resolve(data)
-                } else{
-                  const {active_day_number, avatar_number, achievement_number, spiral_abyss, role_combat} = resp.data.stats
-                  const parsed = {
-                    active_day_number: active_day_number,
-                    avatar_number: avatar_number,
-                    achievement_number: achievement_number,
-                    spiral_abyss: spiral_abyss,
-                    role_combat : role_combat
-                  }
-                  const data = {
-                    uid: game_role_id,
-                    ...parsed,
-                    ...roleInfo
-                  }
-                  resolve(data)
+                switch (game) {
+                  case 'hi3':
+                    break;
+                  case 'gi':
+                    if (detail){
+                      const { world_explorations } = resp.data
+                      const percentage = Math.min((world_explorations.reduce((total, next) => total + next.exploration_percentage, 0) / world_explorations.length / 10000 * 1000).toFixed(1), 100)
+                      const world_exploration = percentage
+      
+                      const data = {
+                        uid: game_role_id,
+                        world_exploration,
+                        ...resp.data.stats,
+                        ...roleInfo
+                      }
+                      resolve(data)
+                    } else{
+                      const {active_day_number, avatar_number, achievement_number, spiral_abyss, role_combat} = resp.data.stats
+                      const parsed = {
+                        active_day_number: active_day_number,
+                        avatar_number: avatar_number,
+                        achievement_number: achievement_number,
+                        spiral_abyss: spiral_abyss,
+                        role_combat : role_combat
+                      }
+                      const data = {
+                        uid: game_role_id,
+                        ...parsed,
+                        ...roleInfo
+                      }
+                      resolve(data)
+                    }
+                    break;
+                  case 'hsr':
+                    break;
+                  case 'zzz':
+                    break;
                 }
-
               } else {
                 logger.error('取得角色詳情介面報錯 %s', JSON.stringify(resp))
                 reject(resp.message)
