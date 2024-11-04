f7693ef48a47ee7c0271aaa526c7af38665be009
侑介 <spark9487111@gmail.com>
2024-11-04 11:25:55 +0800

    add tpl.js

--- utils/tpl.js
+++ utils/tpl.js
@@ -1,4 +1,4 @@
-const gi = (bg, woff2, detail) =>`<svg width="500" height="165" version="1.1" xmlns="http://www.w3.org/2000/svg"
+const GI = (bg, woff2, detail) =>`<svg width="500" height="165" version="1.1" xmlns="http://www.w3.org/2000/svg"
 xmlns:xlink="http://www.w3.org/1999/xlink">
 <title>Genshin Impact User Card</title>
 <foreignObject width="1000" height="330" transform="scale(.5)">
@@ -269,6 +269,238 @@ xmlns:xlink="http://www.w3.org/1999/xlink">
     </body>
 </foreignObject>
 </svg>
-  `
+`
 
-module.exports.gi = gi
+const ZZZ = (bg, woff2, detail) =>`<svg width="500" height="165" version="1.1" xmlns="http://www.w3.org/2000/svg"
+xmlns:xlink="http://www.w3.org/1999/xlink">
+<title>Genshin Impact User Card</title>
+<foreignObject width="1000" height="330" transform="scale(.5)">
+
+    <body xmlns="http://www.w3.org/1999/xhtml">
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                user-select: none;
+            }
+
+            body {
+                width: 100%;
+                height: 100%;
+                font-family: HYWenHei;
+                font-size: 26px;
+                color: #fff;
+            }
+
+            .user-container {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                background-image: url(${bg});
+                background-size: 100%;
+            }
+
+            .top {
+                position: absolute;
+                display: flex;
+                width: 100%;
+                height: 125px;
+                padding-bottom: 20px;
+                background-image: inherit;
+                background-size: 100%;
+                text-shadow: 0px 0px 10px rgba(19, 19, 19, 50%);
+            }
+
+            .top::before,
+            .top::after {
+                content: '';
+                position: absolute;
+                width: 100%;
+                height: 100%;
+            }
+
+            .top::before {
+                background: inherit;
+                filter: blur(3px);
+                height: 100px;
+                opacity: 0.8;
+                z-index: 0;
+            }
+
+            .top::after {
+                background-image: linear-gradient(to bottom, rgba(0, 59, 86, .35), transparent);
+                z-index: 0;
+            }
+
+            .user-info {
+                position: relative;
+                display: inline-block;
+                width: 50%;
+                padding: 4px 20px;
+                z-index: 1;
+            }
+
+            .user-info .name-wrap {
+                display: flex;
+            }
+
+            .name-wrap .name {
+                font-size: 46px;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+
+            .name-wrap .level {
+                margin-top: 12px;
+                margin-left: 8px;
+                font-size: 20px;
+                font-weight: bold;
+            }
+
+            .user-info .uid {
+                font-size: 26px;
+                margin-top: 2px;
+                line-height: 1;
+            }
+
+            .chest-list {
+                position: relative;
+                display: flex;
+                justify-content: flex-end;
+                align-items: center;
+                margin-left: auto;
+                margin-right: 18px;
+                height: 60px;
+                z-index: 1;
+            }
+
+            .bottom {
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+                height: 120px;
+                display: flex;
+                padding-top: 22px;
+                justify-content: space-around;
+                align-items: center;
+                background: inherit;
+                background-position: bottom;
+                text-shadow: 0px 0px 10px rgba(19, 19, 19, 50%);
+            }
+
+            .bottom::before,
+            .bottom::after {
+                content: '';
+                position: absolute;
+                bottom: 0;
+                width: 100%;
+                height: 100%;
+            }
+
+            .bottom::before {
+                background: inherit;
+                height: 100px;
+                opacity: 0.8;
+                filter: blur(3px);
+                z-index: 0;
+            }
+
+            .bottom::after {
+                background-image: linear-gradient(to top, rgba(14, 114, 160, .35), transparent);
+                z-index: 0;
+            }
+
+            .bottom .section {
+                position: relative;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: space-around;
+                min-width: 130px;
+                margin-right: 12px;
+                line-height: 1;
+                z-index: 1;
+            }
+
+            .bottom .section:last-of-type {
+                margin-right: auto;
+            }
+
+            .bottom .section .val {
+                font-size: 48px;
+                letter-spacing: -2px;
+            }
+
+            .bottom .section .val .percent {
+                font-size: 0.8em;
+                margin-left: 2px;
+            }
+
+            .bottom .section .desc {
+                font-size: 20px;
+                margin-top: 4px;
+            }
+
+            .user-container.less .bottom .section.spiral-abyss,
+            .user-container.less .bottom .section.commemorative-coins-number,
+            .user-container.less .chest-list {
+                display: none;
+            }
+
+            @font-face {
+                font-family: HYWenHei;
+                src: url('data:font/woff2;base64,${woff2}') format('woff2');
+            }
+        </style>
+        <div class="user-container ${detail ? '' : 'less'}">
+            <div class="top">
+                <div class="user-info">
+                    <div class="name-wrap">
+                        <div class="name">{{nickname}}</div>
+                        <div class="level">Lv.{{level}}</div>
+                    </div>
+                    <div class="uid">UID: {{uid}}</div>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="section active-days">
+                    <div class="val">{{active_days}}</div>
+                    <div class="desc">活躍天數</div>
+                </div>
+                <div class="section achievement-number">
+                    <div class="val">{{achievement_count}}</div>
+                    <div class="desc">成就達成</div>
+                </div>
+                <div class="section commemorative-coins-number">
+                    <div class="val">{{commemorative_coins}}</div>
+                    <div class="desc">調查協會紀念幣</div>
+                </div>
+                <div class="section avatar-number">
+                    <div class="val">{{avatar_num}}</div>
+                    <div class="desc">代理人數量</div>
+                </div>
+                <div class="section buddy-number">
+                    <div class="val">{{buddy_num}}</div>
+                    <div class="desc">邦布數量</div>
+                </div>
+                <div class="section spiral-abyss">
+                    <div class="val">{{cur_period_zone_layer_count}}</div>
+                    <div class="desc">式輿防衛戰</div>
+                </div>
+                <!-- <div class="section world-exploration">
+                    <div class="val">{{world_exploration}}<span class="text percent">%</span></div>
+                    <div class="desc">世界探索</div>
+                </div> -->
+            </div>
+        </div>
+    </body>
+</foreignObject>
+</svg>
+`
+
+module.exports = {
+    GI,
+    ZZZ
+}
