b2f6653ec9f538269d1dd409ef9e876f79f0aa61
侑介 <spark9487111@gmail.com>
2024-11-15 11:11:46 +0800

    change log var name

--- utils/http.js
+++ utils/http.js
@@ -14,7 +14,7 @@ function http(options) {
   .catch(error => console.error(error))
 }
 
-const webhook = (title, params, color) =>
+const webhook = (title, msg, color) =>
   http({
       method:"POST",
       url: process.env.WEBHOOK,
@@ -22,7 +22,7 @@ const webhook = (title, params, color) =>
           "embeds": [
               {
                   "title": title,
-                  "description": ```${params}```,
+                  "description": "```" + msg + "```",
                   // "color": parseInt(Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0'), 16),
                   "color": color,
                   "timestamp": format(new Date().setHours(-11), 'yyyy-MM-dd HH:mm:ss')
